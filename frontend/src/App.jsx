4607d267ff0a86fa2c96ff9851c6aaf7b1cda401
MithunM20 <mithunmnair103@gmail.com>
2026-07-20 13:46:50 +0530

    little changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,11 @@ function App() {
   return (
     <>
       <Navbar />
-      <section id="home"><Hero /></section>
-      <section id="services"><Services /></section>
-      <section id="about"><About /></section>
-      <section id="pricing"><Pricing /></section>
-      <section id="reviews"><Reviews /></section>
+      <Hero />
+      <Services />
+      <About />
+      <Pricing />
+      <Reviews />
       <Footer />
     </>
   );
